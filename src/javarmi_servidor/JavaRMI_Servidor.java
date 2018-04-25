5df3aa3b918bab20d30521f02b91aaca7cb23f2e
John Haste <joaovictorspteixeira@gmail.com>
2018-04-25 14:53:42 -0300

    Update Names

--- src/javarmi_servidor/JavaRMI_Servidor.java
+++ src/javarmi_servidor/JavaRMI_Servidor.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javarmi;
+package javarmi_servidor;
 
 /**
  *
  * @author John Haste
  */
-public class JavaRMI {
+public class JavaRMI_Servidor {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("oi");
     }
     
 }
